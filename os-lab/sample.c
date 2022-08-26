1a5bc18bb102e83a4f5b8ba7d17ce1245ed671df
204g1a0566 <111738749+204g1a0566@users.noreply.github.com>
2022-08-26 15:30:29 +0530

    my first c file is created

--- os-lab/sample.c
+++ os-lab/sample.c
@@ -1,6 +1,12 @@
 /*
 Name of the experiment : Experiment 1
 Roll no : 204g1a0566
-Name : Navya
+Name : NAVYA
 */
 //source code
+#include<stdio.h>
+int main()
+{
+  printf("HELLO WORLD");
+  return 0
+}
