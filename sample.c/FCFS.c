1da9aa4c1b484dff370c0311e3651bfd6993a29c
204g1a0566 <111738749+204g1a0566@users.noreply.github.com>
2022-09-16 15:21:14 +0530

    L.Navya created sample.c

---
+++ sample.c/FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: L. Navya
+   Roll No: 204G1A0566
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 16-09-2022
+*/
